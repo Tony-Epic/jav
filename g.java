94987d146d92b4aa9c89eb4ee5ad1e530ecce157
Tony-Epic <seljackicigani@gmail.com>
2020-04-20 17:15:36 +0200

    gotovo

--- g.java
+++ g.java
@@ -5,5 +5,9 @@ public class g {
 	public static void main(String[] args) {
 		System.out.println("Napravi metodu");
 	}
-
+	
+		public static void mt()
+		{
+			System.out.println("Gotovo");
+		}
 }
